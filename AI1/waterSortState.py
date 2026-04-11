9cedadd12d37b72aeecc610e0a70f4d1a89929ce
nadia-silva <up202305685@up.pt>
2026-04-11 21:10:31 +0100

    comments in files

--- AI1/waterSortState.py
+++ AI1/waterSortState.py
@@ -1,21 +1,19 @@
-
-
+# Represents an immutable state of the Water Sort puzzle
+# This class is mainly used for search algorithms (BFS, DFS, A*, etc.)
+# It allows states to be stored in sets and dictionaries efficiently
 class WaterSortState:
 
-
     def __init__(self, board, move_history=[]):
+        # Store board as tuple of tuples to make it immutable and hashable
+        # Each tube is represented as a tuple of colors
         self.board = tuple(tuple(bottle) for bottle in board)
-      
 
+    # Hash function allows this object to be used in sets and as dict keys
     def __hash__(self):
-        # to be able to use the state in sets we use tuples for efficiency and performance 
+        # Using tuple-based representation ensures fast and stable hashing
         return hash(self.board)
 
-    
+    # Equality check between two states
     def __eq__(self, other):
-        # compares each tuples of the board
+        # Two states are equal if their board configurations are identical
         return self.board == other.board
-    
-
-
-
